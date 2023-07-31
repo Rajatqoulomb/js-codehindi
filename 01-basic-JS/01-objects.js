2ebbf7071bd8d96e96af8b15e45ac3965cead7fe
Rajatqoulomb <108872295+Rajatqoulomb@users.noreply.github.com>
2023-07-31 09:54:57 +0000

    object change

--- 01-basic-JS/01-objects.js
+++ 01-basic-JS/01-objects.js
@@ -21,7 +21,8 @@ const JsUser = {
 const obj1 = {1: "a", 2:"b"}
 const obj2 = {3: "a", 4:"b"}
 
+
 // const obj3 = Object.assign({}, obj1, obj2);
-console.log(obj3);
+// console.log(obj3);
 const obj3 = {...obj1, ...obj2}
 console.log(obj3);
